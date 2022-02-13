e44552733da7165da51cc5f4348d59f5d8ea8ca2
gus8054 <37838776+gus8054@users.noreply.github.com>
2022-02-13 14:30:41 +0900

    Time Formatting

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -2,6 +2,8 @@ const playBtn = document.getElementById("play");
 const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
+const currentTime = document.getElementById("currentTime");
+const totalTime = document.getElementById("totalTime");
 
 const video = document.querySelector("video");
 
@@ -34,6 +36,22 @@ const handleVolumeRange = (event) => {
   muteBtn.innerText = video.muted ? "Unmute" : "Mute";
 };
 
+const formatTime = (second) =>
+  new Date(second * 1000).toISOString().substring(11, 19);
+
+const handleLoadedMetaData = () => {
+  totalTime.innerText = formatTime(Math.floor(video.duration));
+};
+const handleTimeUpdate = () => {
+  currentTime.innerText = formatTime(Math.floor(video.currentTime));
+};
+
 playBtn.addEventListener("click", handlePlayBtn);
 muteBtn.addEventListener("click", handleMute);
 volumeRange.addEventListener("input", handleVolumeRange);
+video.addEventListener("loadedmetadata", handleLoadedMetaData);
+video.addEventListener("timeupdate", handleTimeUpdate);
+
+if (video.readyState === 4) {
+  handleLoadedMetaData();
+}
